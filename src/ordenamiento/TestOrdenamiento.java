0b8b9668a8498631e4e2508488bb9d7e3827d5d6
valerio <valerio@192.168.1.151>
2021-06-15 09:57:01 -0500

    Algoritmos de ordenamiento y busqueda

--- src/ordenamiento/TestOrdenamiento.java
+++ src/ordenamiento/TestOrdenamiento.java
@@ -5,20 +5,23 @@ public class TestOrdenamiento {
     public static void main(String[] args) {
 
         int[] array = {70, 50, 30, 2, 10};
-
         int[] array2 = {45, 15, 14, 7, 23};
+        int[] array3 = {17, 48, 4, 117, 25};
 
         seleccion(array);
         imprimirArray(array);
 
         System.out.println("");
-
         insercion(array2);
         imprimirArray(array2);
 
+        System.out.println("");
+        burbuja(array3);
+        imprimirArray(array3);
+
     }
 
-    //ARRAY DE SELECCION
+    //ORDENAMIENTO POR SELECCION
     private static void seleccion(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
@@ -39,7 +42,7 @@ public class TestOrdenamiento {
 
     }
 
-    //ARRAY DE INSERCION
+    //ORDENAMIENTO POR INSERCION
     private static void insercion(int[] array) {
 
         for (int i = 1; i < array.length; i++) {
@@ -56,6 +59,21 @@ public class TestOrdenamiento {
             array[j + 1] = aux;
         }
 
+    }
+
+    //ORDENAMIENTO POR BURBUJA
+    private static void burbuja(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int aux = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
+                }
+            }
+
+        }
     }
 
     //MOSTRAR ELEMENTOS DEL ARRAY
