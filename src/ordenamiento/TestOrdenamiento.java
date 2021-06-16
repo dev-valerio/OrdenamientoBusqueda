c7bdd15e5ed053012ca42dc3dae52b70b9f6b658
valerio <valerio@192.168.1.151>
2021-06-16 12:38:31 -0500

    Algoritmos de ordenamiento y busqueda

--- src/ordenamiento/TestOrdenamiento.java
+++ src/ordenamiento/TestOrdenamiento.java
@@ -7,6 +7,7 @@ public class TestOrdenamiento {
         int[] array = {70, 50, 30, 2, 10};
         int[] array2 = {45, 15, 14, 7, 23};
         int[] array3 = {17, 48, 4, 117, 25};
+        int[] array4 = {17, 32, 16, 7, 24};
 
         seleccion(array);
         imprimirArray(array);
@@ -18,6 +19,10 @@ public class TestOrdenamiento {
         System.out.println("");
         burbuja(array3);
         imprimirArray(array3);
+        
+        System.out.println("");
+        quickSort(array4,0,array4.length-1);
+        imprimirArray(array4);
 
     }
 
@@ -74,6 +79,50 @@ public class TestOrdenamiento {
             }
 
         }
+    }
+
+    //ORDENAMIENTO POR QUICKSORT
+    private static void quickSort(int[] num, int izq, int der) {
+
+        int pivote = num[izq];
+
+        int i = izq;
+        int j = der;
+        int aux;
+
+        while (i < j) {
+
+            while (num[i] <= pivote && i < j) {
+                //4
+                i++;
+                //5
+            }
+
+            while (num[j] > pivote) {
+                //5
+                j--;
+                //4        
+            }
+
+            if (i < j) {
+
+                aux = num[i];
+                num[i] = num[j];
+                num[j] = aux;
+            }
+
+        }
+
+        num[izq] = num[j];
+        num[j] = pivote;
+
+        if (izq < j - 1) {
+            quickSort(num, izq, j - 1);
+        }
+        if (j + 1 < der) {
+            quickSort(num, j + 1, der);
+        }
+
     }
 
     //MOSTRAR ELEMENTOS DEL ARRAY
